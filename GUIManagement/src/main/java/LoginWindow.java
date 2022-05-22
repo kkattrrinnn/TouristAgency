03195bb7803a81337b7a2de794a148c5e0225ac4
Your Name <you@example.com>
2022-05-22 22:55:13 +0500

    Added label

--- GUIManagement/src/main/java/LoginWindow.java
+++ GUIManagement/src/main/java/LoginWindow.java
@@ -3,25 +3,35 @@ import java.awt.*;
 
 public class LoginWindow extends GraphicsWindow {
 
-    Font BigFontCS = new Font("ComicSans", Font.BOLD, 45);
+    Font BigFontCS = new Font("ComicSans", Font.BOLD, 40);
     JTextField login;
     JButton ent_button;
+    JLabel label;
+
+    int x = 350 , y = 170, width = 400 , height = 100;
     LoginWindow() {
+
+        this.label= new JLabel("Введите пароль");
+        this.label.setFont(BigFontCS);
+        this.label.setBounds(x-330, y  ,width,height);
+
+        this.jPanel.add(this.label);
+
         this.login = new JTextField();
-        this.login.setBounds(300, 200, 400, 100);
+        this.login.setBounds(x , y ,width,height);
         this.login.setFont(BigFontCS);
-        jPanel.add(this.login);
+        this.jPanel.add(this.login);
 
         this.ent_button = new JButton("Войти");
         this.ent_button.setBounds(400, 400, 400, 100);
         this.ent_button.setBackground(new Color(200, 100, 250, 90));
         this.ent_button.setFont(BigFontCS);
         this.ent_button.addActionListener(e -> {        // обработка нажатия
-            jPanel.removeAll();                         // очистка панели
-            jPanel.repaint();
-            jPanel.revalidate();});
+            this.jPanel.removeAll();                         // очистка панели
+            this.jPanel.repaint();
+            this.jPanel.revalidate();});
 
 
-        jPanel.add(this.ent_button);
+        this.jPanel.add(this.ent_button);
     }
 }
