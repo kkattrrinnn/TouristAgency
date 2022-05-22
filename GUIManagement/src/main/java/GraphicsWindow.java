c13932bc0aa72908504c3e35deb5191b2b6e24d7
kkattrrinnn <katerina_bondar@list.ru>
2022-05-22 15:11:46 +0500

    Some changes

--- GUIManagement/src/main/java/GraphicsWindow.java
+++ GUIManagement/src/main/java/GraphicsWindow.java
@@ -2,7 +2,7 @@ public class GraphicsWindow {
     public static void main(String[] args) {
         createGW();
     }
-    public void createGW() {
-
+    public static void createGW() {
+        System.out.println("Я все починила! Впрочем, ничего нового");
     }
 }
