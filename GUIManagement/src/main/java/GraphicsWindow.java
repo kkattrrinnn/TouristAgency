fb3f7c2a111976f3cfe2e9c4d18cc7228d906baa
Your Name <you@example.com>
2022-05-22 16:49:17 +0500

    Created window

--- GUIManagement/src/main/java/GraphicsWindow.java
+++ GUIManagement/src/main/java/GraphicsWindow.java
@@ -1,8 +1,31 @@
-public class GraphicsWindow {
+import javax.swing.*; // импортируем блиатеку для открывание во окне
+import java.awt.*;
+
+
+public class GraphicsWindow{
     public static void main(String[] args) {
-        createGW();
+        создаем_окно();
     }
-    public static void createGW() {
-        System.out.println("Я все починила! Впрочем, ничего нового");
+
+   public static void создаем_окно(){
+        JFrame jFrame = new JFrame();
+
+       ImageIcon img = new ImageIcon("GUIManagement/img/Icon.jpg");
+       jFrame.setIconImage(img.getImage());  //-- для иконки приложения
+
+        int width = 600;
+        int height = 600;
+
+        jFrame.setVisible(true); //что бы видет окно
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //что закрыть окно
+
+        //ставим окно по центру экрана
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - width / 2,dimension.height / 2 - height / 2  ,width,height);
+        //
+
+        jFrame.setTitle("Tourist Agency"); // даем ему название
+
     }
 }
